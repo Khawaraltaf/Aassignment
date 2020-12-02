5827398604adc237e5a98f94d9c04653d6b0931a
Khawaraltaf Chaudhry <67926531+Khawaraltaf@users.noreply.github.com>
2020-12-02 14:25:48 +0100

    Update CardUI.jsx

--- src/Cads/CardUI.jsx
+++ src/Cads/CardUI.jsx
@@ -5,7 +5,7 @@ const Card = props => {
     return (
         <div className='card text-center shadow'>
             <div className='overflow'>
-                <img src={props.imgsrc} alt="image 1" className='card-img-top' />
+                <img src={props.imgsrc} alt="image1" className='card-img-top' />
             </div>
             <div className="card-body text-dark">
                 <h4 className="card-title">{props.title}</h4>
